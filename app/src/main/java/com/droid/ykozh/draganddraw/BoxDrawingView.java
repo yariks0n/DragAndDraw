65387d382d43ebf322e110fb5162076af23a21a4
Yaroslav Kozhumyaka <y.kozhumyaka@gmail.com>
2018-04-14 15:40:02 +0300

    Start

---
+++ app/src/main/java/com/droid/ykozh/draganddraw/BoxDrawingView.java
@@ -0,0 +1,84 @@
+package com.droid.ykozh.draganddraw;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoxDrawingView extends View {
+
+    private static final String TAG = "BoxDrawingView";
+
+    private Box mCurrentBox;
+    private List<Box> mBoxen = new ArrayList<>();
+
+    private Paint mBoxPaint;
+    private Paint mBackgroundPaint;
+
+    // Используется при создании представления в коде
+    public BoxDrawingView(Context context) {
+        this(context, null);
+    }
+    // Используется при заполнении представления по разметке XML
+    public BoxDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        // Прямоугольники рисуются полупрозрачным красным цветом (ARGB)
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(0x22ff0000);
+        // Фон закрашивается серовато-белым цветом
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(0xfff8efe0);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // Заполнение фона
+        canvas.drawPaint(mBackgroundPaint);
+        for (Box box : mBoxen) {
+            float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+            float right = Math.max(box.getOrigin().x, box.getCurrent().x);
+            float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+            float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+            canvas.drawRect(left, top, right, bottom, mBoxPaint);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(), event.getY());
+        String action = "";
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                mCurrentBox = new Box(current);
+                mBoxen.add(mCurrentBox);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                if (mCurrentBox != null) {
+                    mCurrentBox.setCurrent(current);
+                    invalidate();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                mCurrentBox = null;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                mCurrentBox = null;
+                break;
+        }
+        Log.i(TAG, action + " at x=" + current.x +
+                ", y=" + current.y);
+        return true;
+    }
+}
